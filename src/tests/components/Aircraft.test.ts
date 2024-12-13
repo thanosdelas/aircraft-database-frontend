d00327c03cc60771bc23946c0b871745e5fdcdc6
Thanos Delas <32952288+thanosdelas@users.noreply.github.com>
2024-12-13 15:41:06 +0200

    Aircraft component test with mocked requests (#3)

--- src/tests/components/Aircraft.test.ts
+++ src/tests/components/Aircraft.test.ts
@@ -1,10 +1,36 @@
 import { mount } from '@vue/test-utils';
 import Aircraft from '@/components/Aircraft.vue';
+import AircraftApi from '@/services/aircraft-api';
+import aircraftFixture from '@/tests/fixtures/aircraft.json';
+import aircraftTypesFixture from '@/tests/fixtures/aircraft-types.json';
+import aircraftManufacturersFixture from '@/tests/fixtures/aircraft-manufacturers.json';
+
+import {
+  AircraftResults,
+  AircraftTypeResults,
+  AircraftManufacturerResults,
+  HttpRequestError
+} from '@/types/types';
 
 describe('Aircraft.vue component', () => {
+  beforeAll(() => {
+    global.setTimeout = jest.fn((cb: Function) => cb()) as unknown as typeof setTimeout;
+  });
+
   it('renders the initial message', async() => {
-    const wrapper = await mount(Aircraft);
+    const mockAircraftData = jest.spyOn(AircraftApi.prototype, 'fetchAll').mockResolvedValue(aircraftFixture as AircraftResults);
+    const mockAircraftTypes = jest.spyOn(AircraftApi.prototype, 'fetchAircraftTypes').mockResolvedValue(aircraftTypesFixture as AircraftTypeResults);
+    const mockAircraftManufacturers = jest.spyOn(AircraftApi.prototype, 'fetchAircraftManufacturers').mockResolvedValue(aircraftManufacturersFixture as AircraftManufacturerResults);
+
+    const wrapper = mount(Aircraft);
+    await wrapper.vm.$nextTick();
 
     expect(wrapper.text()).toContain('ManufacturersTypesloading aircraft manufacturers ... Aircraft Models (...) (...)loading aircraft ...');
+
+    await Promise.resolve();
+
+    expect(wrapper.vm.aircraftData.length).toEqual(16);
+    expect(wrapper.vm.aircraftTypes.length).toEqual(12);
+    expect(wrapper.vm.aircraftManufacturers.length).toEqual(11);
   });
 });
